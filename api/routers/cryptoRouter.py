823f710ade6a333ce43202c613d3dc2acbd8a1cd
Artem <rapid9791@gmail.com>
2025-09-18 23:01:16 +0300

    feat: made closed routs (only with JWT token)

--- api/routers/cryptoRouter.py
+++ api/routers/cryptoRouter.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter
-from api.services.coinmarketcap import CryptoCurrencyService
+from fastapi import APIRouter, Depends
 
-router = APIRouter(prefix="/crypto", tags=["crypto"])
+from api.services.coinmarketcap import CryptoCurrencyService
+from api.auth.auth_service import get_current_active_user  # защита через JWT
+from api.auth.auth_model import User
+
+router = APIRouter(
+    prefix="/crypto",
+    tags=["crypto"],
+)
 
 crypto_service = CryptoCurrencyService()
 
 
 @router.get("/")
-async def get_crypto(currency: str):
-    return await crypto_service.get_cryptocurrencies(currency)
+async def get_crypto(
+    currency: str,
+    convert: str = "USD",
+    current_user: User = Depends(get_current_active_user),
+):
+    return await crypto_service.get_cryptocurrencies(crypto=currency, convert=convert)
--- api/routers/weatherRouter.py
+++ api/routers/weatherRouter.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from api.services.weatherService import WeatherClient
+from fastapi import Depends
+from api.auth.auth_service import get_current_active_user
+from api.auth.auth_model import User
 
 router = APIRouter(prefix="/weather", tags=["weather"])
 
@@ -7,5 +10,5 @@ weatherClient = WeatherClient()
 
 
 @router.get("/{city}")
-async def get_weather(city: str):
+async def get_weather(city: str , user: User = Depends(get_current_active_user)):
     return await weatherClient.current_weather(city)
