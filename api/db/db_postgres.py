ca674e74bb5686f6ba1e10c57cd554aefee84281
Artem <rapid9791@gmail.com>
2025-09-24 21:55:14 +0300

    feat: added docker-compose.yml and Dockerfile

--- api/db/db_postgres.py
+++ api/db/db_postgres.py
@@ -14,7 +14,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@db:{DB_PORT}/{DB_NAME}"
 
 # ----------------------------------------------------------------
 
